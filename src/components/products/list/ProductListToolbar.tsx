0905e8c3005452fc396efb6d68f6681e7517073f
Robert Watt <rob.watt@sitecore.com>
2023-03-24 08:43:27 -0500

    ProductListToolbar responsive changes

--- src/components/products/list/ProductListToolbar.tsx
+++ src/components/products/list/ProductListToolbar.tsx
@@ -25,18 +25,32 @@ const ProductListToolbar: FC<ProductListToolbarProps> = ({
   return (
     <>
       <Stack direction="row" mb={5}>
-        <DebouncedSearchInput label="Search products" value={queryParams["Search"]} onSearch={updateQuery("s", true)} />
-        <ProductStatusFilter value={filterParams["Active"]} onChange={updateQuery("active", true)} />
-        <ProductListActions selected={selected} onBulkPromote={onBulkPromote} onBulkEdit={onBulkEdit} />
+        <Stack direction={["column", "column", "column", "row"]}>
+          <DebouncedSearchInput
+            label="Search products"
+            value={queryParams["Search"]}
+            onSearch={updateQuery("s", true)}
+          />
+          <Stack direction="row">
+            <ProductStatusFilter value={filterParams["Active"]} onChange={updateQuery("active", true)} />
+            <ProductListActions selected={selected} onBulkPromote={onBulkPromote} onBulkEdit={onBulkEdit} />
+          </Stack>
+        </Stack>
         <Box as="span" flexGrow="1"></Box>
-        {meta && <ListViewMetaInfo range={meta.ItemRange} total={meta.TotalCount} />}
-        <Box as="span" width="2"></Box>
-        {viewModeToggle}
-        <Link passHref href="/products/new">
-          <Button variant="solid" colorScheme="primary" as="a">
-            Create Product
-          </Button>
-        </Link>
+        <Stack direction={["column", "column", "column", "row"]}>
+          <Stack direction="row" order={[1, 1, 1, 0]}>
+            {meta && <ListViewMetaInfo range={meta.ItemRange} total={meta.TotalCount} />}
+            <Box as="span" width="2"></Box>
+            {viewModeToggle}
+          </Stack>
+          <Box order={[0, 0, 0, 1]} mt={0}>
+            <Link passHref href="/products/new">
+              <Button variant="solid" colorScheme="primary" as="a" mb={3}>
+                Create Product
+              </Button>
+            </Link>
+          </Box>
+        </Stack>
       </Stack>
     </>
   )

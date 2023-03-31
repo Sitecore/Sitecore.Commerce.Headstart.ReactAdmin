9a5d4f67b536a47c77b38fd079593934bfadbd4c
Max Maher <mmaher@four51.com>
2023-03-31 15:42:58 -0500

    Admin Addresses using generic ListView component

--- src/pages/settings/adminaddresses/index.tsx
+++ src/pages/settings/adminaddresses/index.tsx
@@ -1,19 +1,6 @@
-import {Address, AdminAddresses, ListPage} from "ordercloud-javascript-sdk"
-import {Button, ButtonGroup, Container, HStack, Text} from "@chakra-ui/react"
-import {useCallback, useEffect, useMemo, useRef, useState} from "react"
-
-import Card from "components/card/Card"
-import {DataTable} from "components/data-table/DataTable"
-import ExportToCsv from "components/demo/ExportToCsv"
-import {IAdminAddress} from "types/ordercloud/IAdminAddress"
-import {Link} from "components/navigation/Link"
-import {NextSeo} from "next-seo"
-import {OrderCloudTableFilters} from "components/ordercloud-table"
 import ProtectedContent from "components/auth/ProtectedContent"
-import {addressHelper} from "utils/address.utils"
 import {appPermissions} from "constants/app-permissions.config"
-import {useRouter} from "hooks/useRouter"
-import {useSuccessToast} from "hooks/useToast"
+import AdminAddressList from "@/components/adminaddresses/list/AdminAddressList"
 
 /* This declare the page title and enable the breadcrumbs in the content header section. */
 export async function getServerSideProps() {
@@ -30,92 +17,12 @@ export async function getServerSideProps() {
   }
 }
 
-const AdminAddressesPage = () => {
-  const router = useRouter()
-  const successToast = useSuccessToast()
-  const [tableData, setTableData] = useState(null as ListPage<Address>)
-  const [filters, setFilters] = useState({} as OrderCloudTableFilters)
-
-  const fetchData = useCallback(async (filters: OrderCloudTableFilters) => {
-    setFilters(filters)
-    const adminAddresses = await AdminAddresses.List<IAdminAddress>(filters)
-    setTableData(adminAddresses)
-  }, [])
-
-  useEffect(() => {
-    fetchData({})
-  }, [fetchData])
-
-  const deleteAddress = useCallback(
-    async (addressId: string) => {
-      await AdminAddresses.Delete(addressId)
-      await fetchData({})
-      successToast({
-        description: "Address deleted successfully"
-      })
-    },
-    [fetchData, successToast]
-  )
-
-  const columnsData = useMemo(
-    () => [
-      {
-        Header: "ID",
-        accessor: "ID",
-        Cell: ({value, row}) => <Link href={`/settings/adminaddresses/${row.original.ID}`}>{value}</Link>
-      },
-      {
-        Header: "ADDRESS NAME",
-        accessor: "AddressName",
-        Cell: ({value, row}) => <Link href={`/settings/adminaddresses/${row.original.ID}`}>{value}</Link>
-      },
-      {
-        Header: "ADDRESS",
-        Cell: ({row}: {row}) => addressHelper.formatOneLineAddress(row.original)
-      },
-      {
-        Header: "ACTIONS",
-        Cell: ({row}) => (
-          <ButtonGroup>
-            <Button variant="outline" onClick={() => router.push(`/settings/adminaddresses/${row.original.ID}/`)}>
-              Edit
-            </Button>
-            <Button variant="outline" onClick={() => deleteAddress(row.original.ID)}>
-              Delete
-            </Button>
-          </ButtonGroup>
-        )
-      }
-    ],
-    [deleteAddress, router]
-  )
-
-  return (
-    <Container maxW="full">
-      <NextSeo title="Admin Addresses List" />
-      <HStack justifyContent="space-between" w="100%" mb={5}>
-        <Link href={`/settings/adminaddresses/add`}>
-          <Button variant="solid" colorScheme="primary">
-            New Admin Address
-          </Button>
-        </Link>
-        <HStack>
-          <ExportToCsv />
-        </HStack>
-      </HStack>
-      <Card variant="primaryCard">
-        <DataTable data={tableData} columns={columnsData} filters={filters} fetchData={fetchData}></DataTable>
-      </Card>
-    </Container>
-  )
-}
-
-const ProtectedAdminAddressesPage = () => {
+const ProtectedAdminAddresses = () => {
   return (
     <ProtectedContent hasAccess={appPermissions.SettingsManager}>
-      <AdminAddressesPage />
+      <AdminAddressList />
     </ProtectedContent>
   )
 }
 
-export default ProtectedAdminAddressesPage
+export default ProtectedAdminAddresses

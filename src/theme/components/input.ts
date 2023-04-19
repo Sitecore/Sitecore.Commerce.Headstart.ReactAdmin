2809dbb78c553bbdb8b91f79ea74790c004ae751
Eric Sitarz <esitarz@four51.com>
2023-04-19 16:47:35 -0500

    minor fixes

--- src/theme/components/input.ts
+++ src/theme/components/input.ts
@@ -1,27 +1,10 @@
 import {inputAnatomy as parts} from "@chakra-ui/anatomy"
-import { theme } from "@chakra-ui/react"
 import {createMultiStyleConfigHelpers} from "@chakra-ui/styled-system"
 
-const { definePartsStyle, defineMultiStyleConfig } =
-  createMultiStyleConfigHelpers(parts.keys)
-
-const baseStyle = definePartsStyle({
-  field: {
-    _focusVisible:{
-      borderColor: "purple.300",
-      boxShadow: `0 0 0 2px ${theme.colors.purple[300]}`
-    },
-    _focus:{
-      borderColor: "purple.300",
-      boxShadow: `0 0 0 2px ${theme.colors.purple[300]}`
-    },
-    _focusWithin:{
-      borderColor: "purple.300",
-      boxShadow: `0 0 0 2px ${theme.colors.purple[300]}`
-    }
-  },
-})
+const {defineMultiStyleConfig} = createMultiStyleConfigHelpers(parts.keys)
 
 export const Input = defineMultiStyleConfig({
-  baseStyle,
+  defaultProps: {
+    size: "sm"
+  }
 })

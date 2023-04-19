7bfa94aac790d605eb4dfae83230d7194b2d0bc6
Mike Moebeck <mike.moebeck@sitecore.com>
2023-04-19 12:14:53 -0500

    Implement ListView for Buyer Catalogs #76

--- src/pages/buyers/[buyerid]/catalogs/index.tsx
+++ src/pages/buyers/[buyerid]/catalogs/index.tsx
@@ -1,16 +1,7 @@
-import {Box, Button, ButtonGroup, HStack, Switch, Tooltip} from "@chakra-ui/react"
-import {Catalog, Catalogs, ListPage} from "ordercloud-javascript-sdk"
-import {useCallback, useEffect, useMemo, useState} from "react"
-
-import Card from "components/card/Card"
-import {DataTable} from "components/data-table/DataTable"
-import ExportToCsv from "components/demo/ExportToCsv"
-import {ICatalog} from "types/ordercloud/ICatalog"
-import {Link} from "components/navigation/Link"
-import {OrderCloudTableFilters} from "components/ordercloud-table"
+import {Box} from "@chakra-ui/react"
 import React from "react"
 import {useRouter} from "hooks/useRouter"
-import {useSuccessToast} from "hooks/useToast"
+import BuyerCatalogsList from "@/components/buyercatalogs/list/BuyerCatalogsList"
 
 /* This declare the page title and enable the breadcrumbs in the content header section. */
 export async function getServerSideProps() {
@@ -30,119 +21,12 @@ export async function getServerSideProps() {
 
 const CatalogsList = () => {
   const router = useRouter()
-  const successToast = useSuccessToast()
-  const [tableData, setTableData] = useState(null as ListPage<Catalog>)
-  const [filters, setFilters] = useState({} as OrderCloudTableFilters)
-
-  const getCatalogsByBuyerId = async (buyerId: string) => {
-    const assignments = await Catalogs.ListAssignments({buyerID: buyerId})
-    const catalogIds = assignments.Items.map((assignment) => assignment.CatalogID)
-    return await Catalogs.List<ICatalog>({filters: {ID: catalogIds.join("|")}})
-  }
-
-  const fetchData = useCallback(
-    async (filters: OrderCloudTableFilters) => {
-      setFilters(filters)
-      const catalogsList = await getCatalogsByBuyerId(router.query.buyerid as string)
-      setTableData(catalogsList)
-    },
-    [router.query.buyerid]
-  )
-
-  useEffect(() => {
-    fetchData({})
-  }, [fetchData])
-
-  const deleteCatalog = useCallback(
-    async (catalogid: string) => {
-      await Catalogs.Delete(catalogid)
-      fetchData({})
-      successToast({
-        description: "Buyer deleted successfully."
-      })
-    },
-    [fetchData, successToast]
-  )
-
-  const columnsData = useMemo(
-    () => [
-      {
-        Header: "Catalog ID",
-        accessor: "ID",
-        Cell: ({value, row}) => (
-          <Link href={`/buyers/${router.query.buyerid}/catalogs/${row.original.ID}`}>{value}</Link>
-        )
-      },
-      {
-        Header: "Name",
-        accessor: "Name"
-      },
-      {
-        Header: "Description",
-        accessor: "Description"
-      },
-      {
-        Header: "Active",
-        accessor: "Active",
-        Cell: ({value, row}) => (
-          <>
-            <Tooltip label={value ? "Active" : "Non active"} placement="bottom">
-              <Switch colorScheme="teal" size="lg" isReadOnly isChecked={value} />
-            </Tooltip>
-          </>
-        )
-      },
-      {
-        Header: "Category Count",
-        accessor: "CategoryCount",
-        Cell: ({row, value}) => (
-          <Link href={`/buyers/${router.query.buyerid}/catalogs/${row.original.ID}/categories`}>
-            <Button variant="outline">Categories ({value})</Button>
-          </Link>
-        )
-      },
-      {
-        Header: "Marketplace",
-        accessor: "OwnerID"
-      },
-      {
-        Header: "ACTIONS",
-        Cell: ({row}) => (
-          <ButtonGroup>
-            <Button
-              variant="outline"
-              onClick={() => router.push(`/buyers/${router.query.buyerid}/catalogs/${row.original.ID}`)}
-            >
-              Edit
-            </Button>
-            <Button variant="outline" onClick={() => deleteCatalog(row.original.ID)}>
-              Delete
-            </Button>
-          </ButtonGroup>
-        )
-      }
-    ],
-    [deleteCatalog, router]
-  )
+  const buyerid = router.query.buyerid as string
 
   return (
     <>
       <Box pl="GlobalPadding">
-        <HStack justifyContent="space-between" w="100%" mb={5}>
-          <Button
-            onClick={() => router.push(`/buyers/${router.query.buyerid}/catalogs/add`)}
-            variant="solid"
-            colorScheme="primary"
-          >
-            Create catalog
-          </Button>
-          <HStack>
-            <ExportToCsv />
-          </HStack>
-        </HStack>
-        <Card variant="primaryCard">
-          <DataTable data={tableData} columns={columnsData} filters={filters} fetchData={fetchData} />
-        </Card>
+        <BuyerCatalogsList buyerid={buyerid}></BuyerCatalogsList>
       </Box>
     </>
   )

7cfbb81358cc9578356de700bc2340d728dc9d46
Eric Sitarz <esitarz@four51.com>
2023-04-20 14:51:36 -0500

    Use theme colors in the logo

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,12 +1,24 @@
-import { HStack, useColorModeValue, Container } from "@chakra-ui/react"
+import {HStack, useColorModeValue, Container} from "@chakra-ui/react"
 import HeaderLogo from "components/branding/HeaderLogo"
 import AcountNavigation from "components/navigation/AcountNavigation"
+import {MyCommerceLogo} from "../../../public/vector/MyCommerceLogo"
 
 const Header = () => {
   return (
-    <Container as="header" backgroundColor={useColorModeValue("white", "gray.800")} boxShadow="sm" maxW="full" position="sticky" top="0px" zIndex="10" borderBottom={".5px solid"} borderColor="st.borderColor">
+    <Container
+      as="header"
+      backgroundColor={useColorModeValue("white", "gray.800")}
+      boxShadow="sm"
+      maxW="full"
+      position="sticky"
+      top="0px"
+      zIndex="10"
+      borderBottom={".5px solid"}
+      borderColor="st.borderColor"
+    >
       <HStack justifyContent="space-between" alignItems={"center"} h="headerHeight" px={4}>
-        <HeaderLogo />
+        {/* <HeaderLogo /> */}
+        <MyCommerceLogo />
         <AcountNavigation />
       </HStack>
     </Container>

825b8e65169a9719bede48fc1be8f22d05a79511
Eric Sitarz <esitarz@four51.com>
2023-04-20 12:53:36 -0500

    Theming Drawer Revisions
    - Apply Theme closes the drawer
    - reset theme removes localstorage and closes drawer
    - move color mode toggle to theme drawer

--- src/components/navigation/AcountNavigation.tsx
+++ src/components/navigation/AcountNavigation.tsx
@@ -26,28 +26,28 @@ import {
   useColorModeValue,
   useDisclosure
 } from "@chakra-ui/react"
-import { BsMoonStarsFill, BsSun } from "react-icons/bs"
-import { TbDoorExit, TbInbox, TbMoon, TbPalette, TbSun, TbUserCircle } from "react-icons/tb";
-import React, { useState } from "react"
-import { ChevronDownIcon } from "@chakra-ui/icons"
+import {BsMoonStarsFill, BsSun} from "react-icons/bs"
+import {TbDoorExit, TbInbox, TbMoon, TbPalette, TbSun, TbUserCircle} from "react-icons/tb"
+import React, {useState} from "react"
+import {ChevronDownIcon} from "@chakra-ui/icons"
 import Cookies from "universal-cookie"
-import { ItemContent } from "../generic/ItemContent"
+import {ItemContent} from "../generic/ItemContent"
 import ProtectedContent from "../auth/ProtectedContent"
-import { appPermissions } from "constants/app-permissions.config"
-import { useAuth } from "hooks/useAuth"
-import { Link } from "./Link"
-import { ColorPicker } from "../branding/ColorPicker";
-import { LogoUploader } from "../branding/LogoUploader";
-import { FontUploader } from "../branding/FontUploader";
-import { ThemeDrawer } from "./ThemeDrawer";
+import {appPermissions} from "constants/app-permissions.config"
+import {useAuth} from "hooks/useAuth"
+import {Link} from "./Link"
+import {ColorPicker} from "../branding/ColorPicker"
+import {LogoUploader} from "../branding/LogoUploader"
+import {FontUploader} from "../branding/FontUploader"
+import {ThemeDrawer} from "./ThemeDrawer"
 
 const MobileNavigation = () => {
-  const { Logout } = useAuth()
+  const {Logout} = useAuth()
   let usersToken = typeof window !== "undefined" ? localStorage.getItem("usersToken") : ""
   let menuBg = useColorModeValue("white", "navy.800")
-  const { isOpen, onOpen, onClose } = useDisclosure()
+  const {isOpen, onOpen, onClose} = useDisclosure()
   const btnRef = React.useRef()
-  const { colorMode, toggleColorMode } = useColorMode()
+  const {colorMode, toggleColorMode} = useColorMode()
   const color = useColorModeValue("textColor.900", "textColor.100")
   const borderColor = useColorModeValue("white", "gray.700")
   const [selectedOption, setSelectedOption] = useState<String>()
@@ -73,7 +73,25 @@ const MobileNavigation = () => {
       <Menu>
         <MenuButton pos={"relative"}>
           <Icon as={TbInbox} strokeWidth="1.5" fontSize="2xl" />
-          <Badge display={"flex"} alignItems="center" justifyContent={"center"} border={"2px solid"} borderColor={borderColor} color="white" h={"1em"} w={"1em"} borderRadius={"full"} pos={"absolute"} bgColor="red.500" fontSize={"xxs"} p={2} top={"-9px"} left="12px">3</Badge>
+          <Badge
+            display={"flex"}
+            alignItems="center"
+            justifyContent={"center"}
+            border={"2px solid"}
+            borderColor={borderColor}
+            color="white"
+            h={"1em"}
+            w={"1em"}
+            borderRadius={"full"}
+            pos={"absolute"}
+            bgColor="red.500"
+            fontSize={"xxs"}
+            p={2}
+            top={"-9px"}
+            left="12px"
+          >
+            3
+          </Badge>
         </MenuButton>
         <MenuList borderRadius="lg" p={3} display="flex" flexDirection="column" gap={1}>
           <MenuItem borderRadius="lg">
@@ -119,9 +137,7 @@ const MobileNavigation = () => {
               border=".5px solid #ccc"
             />
             <Show above="md">
-              <Text>
-                {usersToken}
-              </Text>
+              <Text>{usersToken}</Text>
               <ChevronDownIcon />
             </Show>
           </HStack>
@@ -138,18 +154,23 @@ const MobileNavigation = () => {
             </MenuItem>
           </ProtectedContent>
           <ProtectedContent hasAccess={appPermissions.MeManager}>
-            <MenuItem onClick={toggleColorMode} icon={<Icon as={colorMode === "dark" ? TbSun : TbMoon} strokeWidth="1.25" fontSize="1.5em" />} lineHeight="0">
-              Color Mode
-            </MenuItem>
-          </ProtectedContent>
-          <ProtectedContent hasAccess={appPermissions.MeManager}>
-            <MenuItem ref={btnRef} onClick={onOpen} _hover={{ textDecoration: "none" }} lineHeight="0"
-              icon={<Icon as={TbPalette} strokeWidth="1.25" fontSize="1.5em" />}>
+            <MenuItem
+              ref={btnRef}
+              onClick={onOpen}
+              _hover={{textDecoration: "none"}}
+              lineHeight="0"
+              icon={<Icon as={TbPalette} strokeWidth="1.25" fontSize="1.5em" />}
+            >
               Theming
             </MenuItem>
           </ProtectedContent>
           <MenuDivider />
-          <MenuItem color="danger.500" onClick={() => Logout()} lineHeight="0" icon={<Icon as={TbDoorExit} display="inline-flex" lineHeight="0" strokeWidth="1.25" fontSize="1.5em" />}>
+          <MenuItem
+            color="danger.500"
+            onClick={() => Logout()}
+            lineHeight="0"
+            icon={<Icon as={TbDoorExit} display="inline-flex" lineHeight="0" strokeWidth="1.25" fontSize="1.5em" />}
+          >
             Log Out
           </MenuItem>
         </MenuList>
--- src/components/navigation/ThemeDrawer.tsx
+++ src/components/navigation/ThemeDrawer.tsx
@@ -1,56 +1,103 @@
 import {
-    Button,
-    DrawerBody,
-    DrawerCloseButton,
-    DrawerContent,
-    DrawerFooter,
-    DrawerHeader,
-    DrawerOverlay,
-    useColorModeValue,
+  Button,
+  ButtonGroup,
+  Divider,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  FormControl,
+  FormLabel,
+  Icon,
+  MenuItem,
+  Switch,
+  useColorMode,
+  useColorModeValue,
+  useModalContext
 } from "@chakra-ui/react"
-import React, { useContext, useState } from "react"
+import React, {useContext, useState} from "react"
 import Cookies from "universal-cookie"
-import { ColorPicker } from "../branding/ColorPicker";
-import { LogoUploader } from "../branding/LogoUploader";
-import { FontUploader } from "../branding/FontUploader";
-import { brandContext } from "../Chakra";
+import {ColorPicker} from "../branding/ColorPicker"
+import {LogoUploader} from "../branding/LogoUploader"
+import {FontUploader} from "../branding/FontUploader"
+import {brandContext, DEFAULT_THEME_COLORS} from "../Chakra"
+import {DEFAULT_THEME_ACCENT, DEFAULT_THEME_PRIMARY, DEFAULT_THEME_SECONDARY} from "theme/foundations/colors"
+import {appPermissions} from "constants/app-permissions.config"
+import {TbSun, TbMoon} from "react-icons/tb"
+import ProtectedContent from "../auth/ProtectedContent"
 
 export const ThemeDrawer = () => {
+  const {colors, setColors} = useContext(brandContext)
+  const [selectedColors, setSelectedColors] = useState(colors)
+  const {colorMode, toggleColorMode} = useColorMode()
+  const {onClose} = useModalContext()
+  const handleColorChange = (colorID: string, newValue: any) => {
+    setSelectedColors((c) => ({...c, [colorID]: newValue}))
+  }
 
-    const { colors, setColors } = useContext(brandContext)
-    const [selectedColors, setSelectedColors] = useState(colors)
+  const [currentColorMode, setCurrentColorMode] = useState(colorMode)
 
-    const handleColorChange = (colorID: string, newValue: any) => {
-        setSelectedColors((c) => ({ ...c, [colorID]: newValue }))
-    }
+  const handleChangeColorMode = () => {
+    setCurrentColorMode((c) => (c === "light" ? "dark" : "light"))
+    setTimeout(() => {
+      toggleColorMode()
+    }, 100)
+  }
 
+  const handleApplyTheme = () => {
+    setColors(selectedColors)
+    onClose()
+  }
 
-    const handleApplyTheme = () => {
-        setColors(selectedColors)
-    }
+  const handleResetTheme = () => {
+    localStorage.removeItem("themeColors")
+    setColors(DEFAULT_THEME_COLORS)
+    onClose()
+  }
 
-    const color = useColorModeValue("textColor.900", "textColor.100")
-    // This function is triggered when the select changes
-    const cookies = new Cookies()
-    let currenttheme
-    if (cookies.get("currenttheme") !== null) {
-        currenttheme = cookies.get("currenttheme")
-    }
-    return (
-        <>
-            <DrawerOverlay />
-            <DrawerContent>
-                <DrawerHeader color={color}>Theming</DrawerHeader>
-                <DrawerBody color={color} display="flex" flexFlow="column nowrap" gap={6}>
-                    <ColorPicker colors={selectedColors} onChange={handleColorChange} />
-                    {/* <LogoUploader />
-                    <FontUploader /> */}
-                </DrawerBody>
-                <DrawerFooter gap={4} justifyContent="center" alignItems={"center"}>
-                    <Button flexGrow="1" variant="solid" size="sm" colorScheme="primary" onClick={handleApplyTheme}>Apply Theming</Button>
-                    <Button as={DrawerCloseButton} variant="outline" size="sm" inset="unset" flex="auto">Discard</Button>
-                </DrawerFooter>
-            </DrawerContent>
-        </>
-    )
+  const color = useColorModeValue("textColor.900", "textColor.100")
+  // This function is triggered when the select changes
+  const cookies = new Cookies()
+  let currenttheme
+  if (cookies.get("currenttheme") !== null) {
+    currenttheme = cookies.get("currenttheme")
+  }
+  return (
+    <>
+      <DrawerOverlay />
+      <DrawerContent>
+        <DrawerCloseButton />
+        <DrawerHeader color={color}>Theming</DrawerHeader>
+        <DrawerBody color={color} display="flex" flexFlow="column nowrap" gap={6}>
+          <ProtectedContent hasAccess={appPermissions.MeManager}>
+            <FormControl display="flex" gap={4}>
+              <Switch
+                isChecked={currentColorMode === "dark"}
+                onChange={handleChangeColorMode}
+                colorScheme={"primary"}
+                id="toggleColorMode"
+                size="lg"
+              />
+              <FormLabel htmlFor="toggleColorMode">Toggle {colorMode} mode</FormLabel>
+            </FormControl>
+          </ProtectedContent>
+          <Divider />
+          <ColorPicker colors={selectedColors} onChange={handleColorChange} />
+          {/* <LogoUploader />
+              <FontUploader /> */}
+        </DrawerBody>
+        <DrawerFooter gap={4} flexFlow="row wrap" justifyContent="center" alignItems={"center"}>
+          <Button variant="solid" colorScheme="primary" onClick={handleApplyTheme} flexGrow={1}>
+            Apply Theming
+          </Button>
+
+          <Button onClick={handleResetTheme} variant="outline" colorScheme="danger" inset="unset">
+            Reset
+          </Button>
+        </DrawerFooter>
+      </DrawerContent>
+    </>
+  )
 }

631ff941c0f0cdfc675e134134a670017bab272e
Crhistian <cramirez@four51.com>
2023-06-21 09:38:19 -0500

    remove unused styles/theme folder
    This includes our old theming files

--- src/styles/theme/playsummit/components/button.ts
+++
@@ -1,7 +0,0 @@
-import type {DeepPartial, Theme} from "@chakra-ui/react"
-
-export const Button: DeepPartial<Theme["components"]["Button"]> = {
-  baseStyle: {
-    borderRadius: "full"
-  }
-}
--- src/styles/theme/playsummit/fonts.ts
+++
@@ -1,6 +0,0 @@
-import type {DeepPartial, Theme} from "@chakra-ui/react"
-
-export const fonts: DeepPartial<Theme["fonts"]> = {
-  body: "Lexend, sans-serif",
-  heading: "Lexend, serif"
-}

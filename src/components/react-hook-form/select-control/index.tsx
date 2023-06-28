5b44c7b6a3af731d7e631b67313600d38f8d908b
Crhistian <cramirez@four51.com>
2023-06-28 11:39:36 -0500

    Update SelectControl component to output string[] and string
    This should make it easier to work with. Also handles both multi-select and single-select

--- src/components/react-hook-form/select-control/index.tsx
+++ src/components/react-hook-form/select-control/index.tsx
@@ -7,7 +7,12 @@ import {ButtonGroup, Button} from "@chakra-ui/react"
 import {TbX} from "react-icons/tb"
 
 export type SelectControlProps = BaseProps & {
-  selectProps?: SelectProps<any, true, any>
+  selectProps?: SelectProps<any, true | false, any>
+}
+
+interface ReactSelectOption {
+  value: string
+  label: string
 }
 
 export const SelectControl: FC<SelectControlProps> = (props: SelectControlProps) => {
@@ -26,37 +31,63 @@ export const SelectControl: FC<SelectControlProps> = (props: SelectControlProps)
     field.onChange(update)
   }
 
+  const handleChange = (options: ReactSelectOption[] | ReactSelectOption) => {
+    if (selectProps?.isMulti) {
+      const updatedOptions = (options as ReactSelectOption[]).map((o) => o.value)
+      field.onChange(updatedOptions)
+    } else {
+      field.onChange((options as ReactSelectOption).value)
+    }
+  }
+
+  const getInitialValue = () => {
+    if (selectProps?.isMulti) {
+      return selectProps.options.filter((option) => field.value.includes(option.value))
+    }
+    if (Array.isArray(field.value)) {
+      throw new Error(
+        "Unexpected array value consider setting isMulti=true on <SelectControl /> if multi value is needed"
+      )
+    }
+    return selectProps.options.find((option) => option.value === field.value)
+  }
+
   return (
     <>
       <FormControl name={name} control={control} label={label} isRequired={isRequired} {...rest}>
         <Select
           {...field}
           {...selectProps}
+          onChange={handleChange}
+          value={getInitialValue()}
           isDisabled={isSubmitting}
           controlShouldRenderValue={!selectProps?.isMulti}
         />
       </FormControl>
-      <ButtonGroup display="flex" flexWrap="wrap" gap={2} marginTop={2}>
-        {(Array.isArray(field.value) ? field.value : []).map((option, index) => (
-          <Button
-            key={index}
-            leftIcon={<TbX />}
-            variant="solid"
-            fontWeight={"normal"}
-            size="sm"
-            borderRadius={"full"}
-            onClick={() => handleRemove(index)}
-            backgroundColor="accent.100"
-            style={{margin: 0}}
-          >
-            {option.label}
-          </Button>
-        ))}
-      </ButtonGroup>
+      {selectProps?.isMulti && (
+        <ButtonGroup display="flex" flexWrap="wrap" gap={2} marginTop={2}>
+          {(Array.isArray(field.value) ? field.value : []).map((optionValue, index) => {
+            const option = selectProps.options.find((o) => o.value === optionValue)
+            return (
+              <Button
+                key={index}
+                leftIcon={<TbX />}
+                variant="solid"
+                fontWeight={"normal"}
+                size="sm"
+                borderRadius={"full"}
+                onClick={() => handleRemove(index)}
+                backgroundColor="accent.100"
+                style={{margin: 0}}
+              >
+                {option.label}
+              </Button>
+            )
+          })}
+        </ButtonGroup>
+      )}
     </>
   )
 }
-
-SelectControl.displayName = "SelectControl"
 
 export default SelectControl

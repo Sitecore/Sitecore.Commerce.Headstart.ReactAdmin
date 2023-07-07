22a9aee3cb288b1aa590ab108877c2e96b5eb6a4
matt-goodman <mag@sitecore.net>
2023-07-07 09:23:04 -0600

    added address to supplier list, add address detail

--- src/components/supplieraddresses/list/SupplierAddressList.tsx
+++ src/components/supplieraddresses/list/SupplierAddressList.tsx
@@ -177,6 +177,7 @@ const SupplierAddressList = () => {
           </Box>
           {renderContent}
           <SupplierAddressDeleteModal
+            supplierID={supplierid}
             onComplete={listViewChildProps.removeItems}
             supplierAddresses={
               actionSupplierAddress

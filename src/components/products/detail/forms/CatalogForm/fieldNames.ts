a0195f4134380188d265e046ecc3083773d5b494
Crhistian <cramirez@four51.com>
2023-07-10 11:49:45 -0500

    PR feedback refactor

--- src/components/products/detail/forms/CatalogForm/fieldNames.ts
+++
@@ -1,2 +0,0 @@
-export const CATALOG_ID = "Catalog.CatalogID"
-export const PRODUCT_ID = "Catalog.ProductID"
--- src/components/products/detail/forms/meta.ts
+++ src/components/products/detail/forms/meta.ts
@@ -10,7 +10,6 @@ import * as pricingForm from "./PricingForm"
 import * as facetsForm from "./FacetsForm"
 import * as mediaForm from "./MediaForm"
 import {ProductDetailTab} from "../ProductDetail"
-import * as catalogForm from "./CatalogForm"
 
 export const defaultValues = {
   ...descriptionForm.defaultValues,
@@ -22,7 +21,6 @@ export const defaultValues = {
   ...mapKeys(pricingForm.defaultValues, (value, key) => `DefaultPriceSchedule.${key}`),
   ...facetsForm.defaultValues,
   ...mediaForm.defaultValues,
-  ...mapKeys(catalogForm.defaultValues, (value, key) => `CatalogAssignments.${key}`),
   Specs: [],
   Variants: [],
   OverridePriceSchedules: [],
@@ -71,5 +69,5 @@ export const tabFieldNames: Record<ProductDetailTab, any[]> = {
   Media: [...values(mediaForm.fieldNames)],
   Facets: [...values(facetsForm.fieldNames)],
   Customization: [],
-  Catalogs: [...values(catalogForm.fieldNames).map((fieldName) => `CatalogAssignments.${fieldName}`)]
+  Catalogs: []
 }

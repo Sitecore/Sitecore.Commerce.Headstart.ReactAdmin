287c6b6e82f409cc63da1ef8ec8c92a1523d07ba
Crhistian <cramirez@four51.com>
2023-07-19 13:53:49 -0500

    PR feedback, make quantity to return dependent on quantity shipped #179

--- src/components/orders/detail/order-returns/return-modal/ReturnItemsTable.tsx
+++ src/components/orders/detail/order-returns/return-modal/ReturnItemsTable.tsx
@@ -45,7 +45,7 @@ export function ReturnItemsTable({control, lineItems, allOrderReturns, existingR
         .map((orderReturn) => orderReturn.ItemsToReturn.filter((returnItem) => returnItem.LineItemID === lineItem.ID))
     )
     const quantityAlreadyReturned = otherReturnItems.reduce((acc, returnItem) => acc + returnItem.Quantity || 0, 0)
-    return lineItem.Quantity - quantityAlreadyReturned
+    return lineItem.QuantityShipped - quantityAlreadyReturned
   }
 
   const buildQuantityOptions = (lineItem: ILineItem) => {

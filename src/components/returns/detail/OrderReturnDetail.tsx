cc629115040409897650bc5de2c2f30c0dc23217
Crhistian <cramirez@four51.com>
2023-07-25 10:32:07 -0500

    Add the ability to complete return
    this was accidentally removed during the order return detail page refactor

--- src/components/returns/detail/OrderReturnDetail.tsx
+++ src/components/returns/detail/OrderReturnDetail.tsx
@@ -1,19 +1,70 @@
 import {OrderStatus} from "@/components/orders/OrderStatus"
 import {ReturnItemsTable} from "./ReturnItemsTable"
 import {HeaderItem} from "@/components/shared/HeaderItem"
-import {Container, Heading, VStack, Card, CardBody, Flex, Stack, Text, CardHeader, Box} from "@chakra-ui/react"
+import {
+  Container,
+  Heading,
+  VStack,
+  Card,
+  CardBody,
+  Stack,
+  Text,
+  CardHeader,
+  Box,
+  HStack,
+  Button
+} from "@chakra-ui/react"
 import {useOrderReturnDetail} from "hooks/useOrderReturnDetail"
 import {dateHelper, priceHelper} from "utils"
 import {Link} from "@/components/navigation/Link"
+import {Payment, Payments} from "ordercloud-javascript-sdk"
+import {useState} from "react"
 
 type OrderReturnDetailProps = ReturnType<typeof useOrderReturnDetail>
 
-export function OrderReturnDetail({orderReturn, lineItems}: OrderReturnDetailProps) {
+export function OrderReturnDetail({orderReturn, lineItems, payment, fetchOrderReturn}: OrderReturnDetailProps) {
+  const [loading, setLoading] = useState(false)
   const cardGap = 3
+
+  const handleCompleteReturn = async () => {
+    try {
+      setLoading(true)
+      let orderReturnPayment: Payment
+      if (payment) {
+        // we must create a payment with a negative amount equal to OrderReturn.RefundAmount in order for the order
+        // return to complete automatically
+        orderReturnPayment = {
+          ...payment,
+          OrderReturnID: orderReturn.ID,
+          Amount: orderReturn.RefundAmount * -1
+        }
+      } else {
+        // in ordercloud it is possible to submit an order without payment, so we must account for this
+        orderReturnPayment = {
+          Type: null,
+          OrderReturnID: orderReturn.ID,
+          Amount: orderReturn.RefundAmount * -1,
+          Accepted: true
+        }
+      }
+      await Payments.Create("All", orderReturn.OrderID, orderReturnPayment)
+      await fetchOrderReturn(orderReturn.ID) // get updated status
+    } finally {
+      setLoading(false)
+    }
+  }
+
   return (
     <Container maxW="100%" bgColor="st.mainBackgroundColor" flexGrow={1} p={[4, 6, 8]}>
       <Heading size="md" marginBottom={7}>
-        Return #{orderReturn.ID}
+        <HStack justifyContent="space-between">
+          <Text>Return #{orderReturn.ID}</Text>
+          {orderReturn.Status === "Open" && (
+            <Button variant="solid" colorScheme="primary" onClick={handleCompleteReturn} isLoading={loading}>
+              Complete
+            </Button>
+          )}
+        </HStack>
       </Heading>
       <VStack gap={cardGap} width="full">
         <Card width="full">
--- src/hooks/useOrderReturnDetail.tsx
+++ src/hooks/useOrderReturnDetail.tsx
@@ -1,12 +1,14 @@
 import {useRouter} from "next/router"
-import {LineItems, OrderReturns} from "ordercloud-javascript-sdk"
+import {LineItems, OrderReturns, Payments} from "ordercloud-javascript-sdk"
 import {useState, useEffect, useCallback} from "react"
 import {ILineItem} from "types/ordercloud/ILineItem"
 import {IOrderReturn} from "types/ordercloud/IOrderReturn"
+import {IPayment} from "types/ordercloud/IPayment"
 
 export function useOrderReturnDetail() {
   const {isReady, query} = useRouter()
   const [orderReturn, setOrderReturn] = useState(null as IOrderReturn)
+  const [payment, setPayment] = useState(null as IPayment)
   const [lineItems, setLineItems] = useState([] as ILineItem[])
   const [loading, setLoading] = useState(true)
 
@@ -25,10 +27,20 @@ export function useOrderReturnDetail() {
     return response
   }, [])
 
+  const fetchPayment = useCallback(async (orderReturn: IOrderReturn) => {
+    // get a list of payments that aren't order return payments
+    const paymentList = await Payments.List("All", orderReturn.OrderID, {
+      filters: {Accepted: true, OrderReturnID: "!*"}
+    })
+    if (paymentList.Items.length > 0) {
+      setPayment(paymentList.Items[0])
+    }
+  }, [])
+
   const loadPageData = useCallback(async () => {
     const _return = await fetchOrderReturn(query.returnid as string)
-    await fetchLineItems(_return)
-  }, [query.returnid, fetchOrderReturn, fetchLineItems])
+    await Promise.all([fetchLineItems(_return), fetchPayment(_return)])
+  }, [query.returnid, fetchOrderReturn, fetchLineItems, fetchPayment])
 
   useEffect(() => {
     const initializeData = async () => {
@@ -48,9 +60,11 @@ export function useOrderReturnDetail() {
     // order return data
     orderReturn,
     lineItems,
+    payment,
 
     // order return actions
     fetchOrderReturn,
-    fetchLineItems
+    fetchLineItems,
+    fetchPayment
   }
 }

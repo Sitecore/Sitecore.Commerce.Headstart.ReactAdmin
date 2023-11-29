e43abdf712087a5d8d338a816e60a067448c6554
Crhistian <cramirez@four51.com>
2023-11-29 12:19:56 -0600

    Update dashboard to pull in real ordercloud data if configured to do so
    If NEXT_PUBLIC_USE_REAL_DASHBOARD_DATA is set to true then the dashboard will show real OrderCloud data.
    
    Note that the graph is not possible to implement without a backend component due to the sheer amount of API calls that would be required. If this type of analytics is required its recommended to compound that data on the backend separately and then simply retrieve the results client-side.
    
    The data displayed has been modified slightly as well. Instead of monthly intervals we are showing weekly

--- src/components/analytics/PercentChangeTile.tsx
+++ src/components/analytics/PercentChangeTile.tsx
@@ -1,28 +1,32 @@
 import {TriangleDownIcon, TriangleUpIcon} from "@chakra-ui/icons"
-import {
-  Flex,
-  Card,
-  Text,
-  Box,
-  Icon,
-  useColorModeValue,
-  CardHeader,
-  CardBody,
-  Heading,
-  IconButton
-} from "@chakra-ui/react"
+import {Card, Text, useColorModeValue, CardBody, Heading} from "@chakra-ui/react"
 import React from "react"
-import {TbArrowsDiagonal, TbArrowsDiagonal2, TbLayoutNavbarExpand, TbLayoutSidebarLeftExpand} from "react-icons/tb"
-import schraTheme from "theme/theme"
+import {priceHelper} from "utils"
 
-export default function PercentChangeTitle(prop) {
+interface PercentChangeTileProps {
+  previousAmount: number
+  currentAmount: number
+  title: string
+  label: string
+  icon: any
+  isMoney?: boolean
+}
+export default function PercentChangeTile({
+  previousAmount,
+  currentAmount,
+  title,
+  label,
+  isMoney = true
+}: PercentChangeTileProps) {
   const color = useColorModeValue("blackAlpha.500", "whiteAlpha.500")
   const labelColor = useColorModeValue("blackAlpha.400", "whiteAlpha.500")
   const borderColorPos = useColorModeValue("green.300", "green.700")
   const borderColorNeg = useColorModeValue("red.100", "red.800")
+  const percentchange = Math.round(((currentAmount - previousAmount) / previousAmount) * 100)
+  const percentchangetype = percentchange > 0 ? "pos" : "neg"
 
   return (
-    <Card w="full" border=".5px solid" borderColor={prop.percentchangetype === "pos" ? borderColorPos : borderColorNeg}>
+    <Card w="full" border=".5px solid" borderColor={percentchangetype === "pos" ? borderColorPos : borderColorNeg}>
       <CardBody display="flex" flexFlow="column nowrap" pos={"relative"}>
         <Text
           fontSize="5xl"
@@ -32,10 +36,10 @@ export default function PercentChangeTitle(prop) {
           display="inline-flex"
           alignItems="center"
         >
-          {prop.totalamount}
+          {isMoney ? priceHelper.formatShortPrice(currentAmount) : currentAmount}
         </Text>
-        <Heading fontSize="lg" mb="6px" textTransform="capitalize" mt={"auto"}>
-          {prop.title}
+        <Heading fontSize="lg" mb="6px" mt={"auto"}>
+          {title}
         </Heading>
         <Text
           as="span"
@@ -47,19 +51,18 @@ export default function PercentChangeTitle(prop) {
           alignItems="center"
           gap={1}
         >
-          {prop.percentchangetype === "pos" ? (
+          {percentchangetype === "pos" ? (
             <Text as="span" color="green.400" fontWeight="bold" display="inline-flex" alignItems="center" gap={1}>
-              <TriangleUpIcon /> {prop.percentchange}%
+              <TriangleUpIcon /> {percentchange}%
             </Text>
           ) : (
             <Text as="span" color="red.400" fontWeight="bold" display="inline-flex" alignItems="center" gap={1}>
               <TriangleDownIcon />
-              {prop.percentchange}%
+              {percentchange}%
             </Text>
           )}{" "}
-          {prop.percentlabel}
+          {label}
         </Text>
-        {/* {prop.icon} */}
       </CardBody>
     </Card>
   )
